39682984d4936e7868e7da97f0ac3d9eb65abe2d
gabriel.tozatto <gabriel.tozatto@hotmail.com>
2021-12-21 13:26:25 -0300

    removendo pastas antigas

--- Calendario-Autoplay/routes/disciplina/index.js
+++
@@ -1,9 +0,0 @@
-const rotasDisciplina = require('express').Router()
-
-rotasDisciplina.get('/', (req, res) => {
-    res.render('../views/disciplina', {
-        title: 'Disciplinas'
-    })
-})
-
-module.exports = rotasDisciplina 
--- README.md
+++
@@ -1,2 +0,0 @@
-Este é um projeto desenvolvido na equipe da Autoplay.
-Possui como objetivo realizar agendamentos de salas disponíveis de acordo com as datas do calendário presente no site.# Calendario_Autoglass
