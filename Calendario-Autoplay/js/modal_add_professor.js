5d303d11cfbe7c73fc175e8627e74d1039a25bd2
Gabriel Tozatto <63324290+gabrieltozatto21@users.noreply.github.com>
2021-12-21 13:02:28 -0300

    Delete Calendario-Autoplay directory

--- Calendario-Autoplay/js/modal_add_professor.js
+++
@@ -1,8 +0,0 @@
-function cadastrar(){
-    let modal = document.querySelector('.modal')
-    modal.style.display = 'flex';
-}
-function fechar(){
-    var modal = document.querySelector('.modal')
-    modal.style.display = 'none';
-}
--- Calendario-Autoplay/routes/turma/index.js
+++
@@ -1,9 +0,0 @@
-const rotasTurma = require('express').Router()
-
-rotasTurma.get('/', (req, res) => {
-    res.render('../views/turmas', {
-        title: 'Turmas'
-    })
-})
-
-module.exports = rotasTurma 
