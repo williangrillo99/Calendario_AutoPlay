3d525e63c06ebf3ef54c9316435f3fa5e84b3021
Hausenbauer <87825507+Hausenbauer@users.noreply.github.com>
2022-01-24 16:55:28 -0300

    Documentando o código e Criando modal de eventos

--- api-agenda/api/public/js/calendario/modalCalendario.js
+++ api-agenda/api/public/js/calendario/modalCalendario.js
@@ -1,9 +1,9 @@
 let modal = document.querySelector('#visualizar')
 
-function fechar(){
+function fechar(){ //Criando função para fechar o modal
     modal.style.display = 'none';
 }
 
-window.onclick = function(event) {
+window.onclick = function(event) { // Função para dar à função acima um uso 
     if (event.target == modal) fechar()
 }
