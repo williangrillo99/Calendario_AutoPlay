36233dcb28fffc608511d03ee720dacb779d683c
Willian Grillo <williangrillo@outlook.com.br>
2022-01-25 12:01:35 -0300

    Checkbox Semanal

--- api-agenda/api/public/js/index/jsValidarCheckBox.js
+++
@@ -1,8 +0,0 @@
-var checkboxreccorencia = document.querySelector('#verificaRecorrencia')
-
-console.log(checkboxreccorencia.Checked)
-
-if(checkboxreccorencia.Checked){
-    console.log("entrou")
-    alert("adsdhsa")
-}
